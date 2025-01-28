3eabc379e9031cb3b1b1824d389f78113cb9564b
Amaan Khan <amaanbkhan57@gmail.com>
2025-01-28 17:56:21 -0600

    Added Name and Section

--- HW1.java
+++ HW1.java
@@ -1,6 +1,6 @@
 
 /*
- * *** PLACE YOUR NAME / SECTION  HERE ***
+ * *** Amaan Khan / 001 ***
  *
  * Homework # 1 (Programming Assignment). This Java class defines some basic
  * manipulation operations on Linked-Lists and Stacks.
